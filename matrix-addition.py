6067b71f32d5e5996b4fe75e9b8f44f08523de25
Quentin VIVIEN <112775431+quentinvvn@users.noreply.github.com>
2022-09-14 19:26:17 +0200

    Create matrix-addition.py

---
+++ matrix-addition.py
@@ -0,0 +1,32 @@
+from msvcrt import getch
+
+first = [[0,0,0],
+         [0,0,0],
+         [0,0,0]]
+
+print("\nEntrez les éléments de la première matrix :")
+for i in range(len(first)):
+   for j in range(len(first[0])):
+       first[i][j] = int(input(">"))
+
+second = [[0,0,0],
+          [0,0,0],
+          [0,0,0]]
+
+print("\nEntrez les éléments de la seconde matrix :")
+for i in range(len(first)):
+   for j in range(len(first[0])):
+       second[i][j] = int(input(">"))
+
+add = [[0,0,0],
+       [0,0,0],
+       [0,0,0]]
+
+for i in range(len(first)):
+   for j in range(len(first[0])):
+       add[i][j] = first[i][j] + second[i][j]
+
+print("\nRésultat de l'addition des deux matrices :\n")
+for r in add:
+   print(r)
+getch()
